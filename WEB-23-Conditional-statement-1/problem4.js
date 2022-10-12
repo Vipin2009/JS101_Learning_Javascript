b4287a900319731419980fbcecb0fcdfb051fac4
Vipin2009 <nanuverma042@gmail.com>
2022-10-12 21:54:19 +0530

    day2,day3,day4

---
+++ WEB-23-Conditional-statement-1/problem4.js
@@ -0,0 +1,13 @@
+let database_user = "nanuverma042@gmail.com";
+let database_pass = "xyz12345";
+
+let user = "nanuverma042@gmail.com";
+let pass = "xyz12345";
+if (database_user==user){
+  if (database_pass==pass){
+    console.log("Login successful");
+  }
+  else{
+    console.log("Wrong Credentials");
+  }
+}
---
+++ WEB-23-Conditional-statement/problem2.js
@@ -0,0 +1,6 @@
+let age = 16;
+if (age>=16){
+  console.log("Apply for a license");
+}else{
+  console.log("NA");
+}
